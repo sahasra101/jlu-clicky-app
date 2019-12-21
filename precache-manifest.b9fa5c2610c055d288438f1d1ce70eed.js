eaa5f06687ec05110a5767ac36829bb46b741685
sahasra101 <ajaykiri7@gmail.com>
2019-12-20 19:04:49 -0500

    Updates

---
+++ precache-manifest.b9fa5c2610c055d288438f1d1ce70eed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b740a66933370b86cbe257a794a3160",
+    "url": "/jlu-clicky-app/index.html"
+  },
+  {
+    "revision": "07fe028738208aa822d8",
+    "url": "/jlu-clicky-app/static/css/main.ea904149.chunk.css"
+  },
+  {
+    "revision": "09846fba42d9a9a4e887",
+    "url": "/jlu-clicky-app/static/js/2.53727795.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/jlu-clicky-app/static/js/2.53727795.chunk.js.LICENSE"
+  },
+  {
+    "revision": "07fe028738208aa822d8",
+    "url": "/jlu-clicky-app/static/js/main.447b31ce.chunk.js"
+  },
+  {
+    "revision": "a615343bb7579f806a1f",
+    "url": "/jlu-clicky-app/static/js/runtime-main.e894ca0b.js"
+  }
+]);
